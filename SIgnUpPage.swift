e7b126b874298e3b92f5ba46b538d257aead88e1
Marc Mullally <marcmullally0@gmail.com>
2024-01-29 12:12:05 -0500

    gg

---
+++ SIgnUpPage.swift
@@ -0,0 +1,29 @@
+//
+//  SIgnUpPage.swift
+//  Savori
+//
+//  Created by 2b on 1/29/24.
+//
+
+import UIKit
+
+class SIgnUpPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
